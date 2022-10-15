72baec1c1ed546131d8367b6e0492416c31d4db1
xP4ST3x <matias.past@gmail.com>
2022-10-15 18:35:52 -0300

    aaaa

--- my-app/components/Ubicacion.js
+++ my-app/components/Ubicacion.js
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
-
 import * as Location from 'expo-location';
 import { Marker } from 'react-native-maps';
 
 export default function Ubicacion() {
   const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
   const [latitud, setLatitud] = useState(0);
   const [longitud, setLongitud] = useState(0);
   useEffect(() => {
@@ -20,33 +17,20 @@ export default function Ubicacion() {
 
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
+      JSON.stringify(location);
+      setLatitud(location.coords.latitude);
+      setLongitud(location.coords.longitude);
     })();
   }, []);
 
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-    console.log(text)
-  }
+
 
   return (
-    <View style={styles.container}>
+    <>
       <Marker coordinate={{
         latitude: latitud,
         longitude: longitud
       }}></Marker>
-      <Text>{text}</Text>
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
